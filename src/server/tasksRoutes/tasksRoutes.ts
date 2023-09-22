614ef6aff9a0fa5c313943a23b70e26484e78b36
Thomás Dixini <thomasdixini@gmail.com>
2023-09-22 00:10:10 -0300

    :sparkles: feat: Criando primeira task
    Criando rotas POST e GET para criação e listagem das TASKs

--- src/server/tasksRoutes/tasksRoutes.ts
+++ src/server/tasksRoutes/tasksRoutes.ts
@@ -1,7 +1,34 @@
-import { type FastifyInstance } from "fastify";
+import { randomUUID } from "crypto";
+import { FastifyRequest, FastifyInstance, FastifyReply } from "fastify";
+interface Task {
+  title: string;
+  description: string;
+}
+
+const fakeDB: Task[] = [];
 
 export async function tasksRoutes(app: FastifyInstance) {
-  app.get("/", async (req, reply) => {
-    return reply.status(200).send("Hello world!");
+  app.post("/", async (req: FastifyRequest, reply: FastifyReply) => {
+    const { title, description } = req.body as Task;
+
+    const task = {
+      id: randomUUID(),
+      title,
+      description,
+      created_at: new Date(),
+      updated_at: new Date(),
+    };
+    fakeDB.push(task);
+    return reply.status(201).send("Task created successfully");
+  });
+
+  app.get("/", async (req: FastifyRequest, reply: FastifyReply) => {
+    return (
+      reply.status(200),
+      {
+        message: "Task listed successfully",
+        fakeDB,
+      }
+    );
   });
 }
