7ee53b81ded90fb46b118d61eaebc35b77e9a38e
Thomás Dixini <thomasdixini@gmail.com>
2023-10-09 00:39:33 -0300

    :recycle: refactor: Colocando variaveis de ambiente na config do meu Knex

--- src/database/config.ts
+++ src/database/config.ts
@@ -1,13 +1,14 @@
 import knex, { Knex } from "knex";
+import "dotenv/config";
 
 export const config: Knex.Config = {
   client: "mysql2",
   connection: {
     host: "127.0.0.1",
     port: 3301,
-    user: "root",
-    password: "secret",
-    database: "dev_database",
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
   },
   migrations: {
     extension: "ts",
